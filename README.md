14f1395956d612c5767dfd520ed29b282bc8c86c
bertolo1988 <tiagobertolo@gmail.com>
2017-02-04 11:12:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# expressjs-hello-world
+
+An expressjs server with only one hello world route.
+
+## run
+
+`port=? npm start`
---
+++ server.js
@@ -0,0 +1,11 @@
+let express = require('express')
+var app = express()
+var port = process.env.port || 3010;
+
+app.get('/', function(req, res) {
+	res.send('Hello World!')
+})
+
+app.listen(port, function() {
+	console.log('Example app listening on port ' + port)
+})
